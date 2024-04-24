0be0ed8eefeb19422b1267ad22a53cfa646f70a3
alexmolinaws <info@alexmolinaws.com>
2024-04-24 09:46:42 -0400

    Licence, branding and desktop styles complete

--- app.js
+++ app.js
@@ -1,1 +1,28 @@
+
+
+function getUserName() {
+    let userName = prompt("What's your name?");
+    document.getElementById("user-name").innerHTML=userName;
+    return;
+};
+
+function calcBMI() {
+    let weight = document.getElementById("weight");
+    let height = 180;
+    let result = weight / (height * height);
+    
+    // if () {
+
+    // } if else () {
+
+    // } if else () {
+
+    // } if else () {
+
+    // } else {
+
+    // }
+
+}
+
 console.log("JS working")
